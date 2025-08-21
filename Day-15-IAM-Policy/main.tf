ea32f8562b6d95cc3ff51a0231fa3a49f0d63916
David Nanjila <nanjiladavid2@gmail.com>
2025-08-21 22:36:57 +0300

    Create IAM policy using terraform solving day 15 challenge
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

---
+++ Day-15-IAM-Policy/main.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_policy" "my_policy" {
+  name        = "iampolicy_ravi"
+  description = "Allow read-only access to the EC2 console, i.e., this policy must allow users to view all instances, AMIs, and snapshots in the Amazon EC2 console"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+          "ec2:List*",
+          "ec2:Get*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
